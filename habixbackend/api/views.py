367d40330d3b51569f4277340064e918f47e28b9
Tiqur <Tiqur7@gmail.com>
2021-01-03 16:47:51 -0800

    Added routing and api methods

--- habixbackend/api/views.py
+++ habixbackend/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .models import HabixUser, DailyTask
+from habix.serializers import HabixUserSerializer, DailyTaskSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+class HabixUserList(ListCreateAPIView):
+    queryset = HabixUser.objects.all()
+    serializer_class = HabixUserSerializer
+
+
+class HabixUserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = HabixUser.objects.all()
+    serializer_class = HabixUserSerializer
+
+
+class DailyTaskList(ListCreateAPIView):
+    queryset = DailyTask.objects.all()
+    serializer_class = DailyTaskSerializer
+
+
+class DailyTaskDetail(RetrieveUpdateDestroyAPIView):
+    queryset = DailyTask.objects.all()
+    serializer_class = DailyTaskSerializer
